8058a7e1271c1521b673b407a00f56fdf1d94725
benwayne182 <benwayne182@gmail.com>
2016-03-04 11:56:38 +0100

    adding some new tests

---
+++ js/ex3.js
@@ -0,0 +1,17 @@
+var app3 = angular.module('app3',[]);
+
+app3.controller('gListCtrl', function($scope){
+  $scope.groceries= [
+
+    {item: "Tomatoes", purchased: false},
+    {item: "Water", purchased: false},
+    {item: "Potatoes", purchased: false},
+    {item: "Bread", purchased: false},
+  ];
+
+  // Return a different HTML partial based on whether the
+  // checkbox is checked or not
+  $scope.getList = function(){
+    return $scope.showList ? "ulgrocerylist.html" : "grocerylist.html";
+  };
+});
