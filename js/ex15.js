29e494c0d3ade3f36327a5b7e403e7fd51679353
benwayne182 <benwayne182@gmail.com>
2016-03-08 15:40:40 +0100

    add tuto 15

---
+++ js/ex15.js
@@ -0,0 +1,29 @@
+var app15 = angular.module('app15', []);
+
+app15.controller('mainCtrl', function($scope, $http) {
+
+  $scope.getData = function() {
+    $http.get("playerData.json").success(
+      function(data) {
+        $scope.players = data;
+      }
+    );
+  }
+
+  // Get this Chrome extension if you get the
+  // CORS error
+  // Allow-Control-Allow-Origin
+  $scope.translate = function() {
+    var words = $scope.wordsToTranslate.replace(/ /g, "+");
+
+    var jsonUrl = "http://newjustin.com/translateit.php?action=translations&english_words=" + words;
+
+    $http.get(jsonUrl).success(
+      function(data) {
+        $scope.translated = data;
+      }
+    );
+
+  }
+
+});
