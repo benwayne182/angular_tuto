b55852d0494da81fe3e02ca5b657300447dbf071
benwayne182 <benwayne182@gmail.com>
2016-03-08 13:08:18 +0100

    update tuto 14

--- js/ex14.js
+++ js/ex14.js
@@ -15,4 +15,74 @@ app14.controller('mainCtrl', function($scope, $window, $location,
   $location.search("random=stuff");
   $scope.currSearch = $location.search();
 
+  $interval(function() {
+    var hour = new Date().getHours();
+
+    var minutes = ('0' + new Date().getMinutes()).slice(-2);
+
+    var seconds = ('0' + new Date().getSeconds()).slice(-2);
+
+    $scope.time = hour + ":" + minutes + ":" + seconds
+
+
+  }, 2000);
+
+  $scope.$log = $log;
+
+  $scope.throwException = function() {
+    try {
+      throw new Error("Exception Thrown");
+    } catch (e) {
+      $exceptionHandler(e.message, "Caught Exception");
+    }
+  };
+
+
+  $scope.badStuff = "";
+
+  // Monitors a change in value for badStuff
+  // and sanitizes it
+  $scope.$watch("badStuff", function(value) {
+    $scope.htmlData = $sanitize(value);
+  });
+
+
+  $scope.players = [{
+    name: "Barry Bonds",
+    avg: 0.298,
+    hr: 762,
+    obp: 0.444
+  }, {
+    name: "Hank Aaron",
+    avg: 0.305,
+    hr: 755,
+    obp: 0.374
+  }, {
+    name: "Babe Ruth",
+    avg: 0.342,
+    hr: 714,
+    obp: 0.474
+  }, {
+    name: "Ted Williams",
+    avg: 0.344,
+    hr: 521,
+    obp: 0.482
+  }];
+
+});
+
+app14.directive("getPlayerInfo", function($compile) {
+  return function(scope, element, attrs) {
+    var playerList = "<ul><li ng-repeat='player in players'>{{player.name}}</li></ul>";
+
+    var listElem = angular.element(playerList);
+
+    var compileFunc = $compile(listElem);
+
+    compileFunc(scope);
+
+    element.append(listElem);
+
+
+  }
 });
